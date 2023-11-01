1288ed32040f7e34d063ed43b230870c287d44fd
Silleellie <antonio.silletti@gmail.com>
2023-11-01 23:18:45 +0100

    separated in proper module abstract metric

--- src/evaluate/metrics/metrics.py
+++ src/evaluate/metrics/metrics.py
@@ -2,108 +2,11 @@ from __future__ import annotations
 
 import operator
 import time
-from abc import ABC, abstractmethod
-from collections.abc import Collection
-from typing import Callable, List
+from typing import Callable
 
 import numpy as np
-from requests.structures import CaseInsensitiveDict
 
-
-class PaddedArr(np.ndarray):
-    def __new__(cls, iterable: Collection[Collection[str]], *args, **kwargs):
-        # Find the maximum length of the sublists
-        max_len = max(len(sublist) for sublist in iterable)
-
-        # Create a new NumPy array filled with <PAD> token
-        padded_array = np.full((len(iterable), max_len), fill_value="<PAD>")
-
-        # Copy the data from the original list into the new array
-        for i, sublist in enumerate(iterable):
-            padded_array[i, :len(sublist)] = sublist
-
-        return padded_array.astype(str)
-
-
-class Metric(ABC):
-
-    # name - class mapping, used for when metrics should be initialized from strings
-    str_alias_cls: dict = CaseInsensitiveDict()
-
-    # automatically called on subclass definition, will populate the str_alias_cls dict
-    def __init_subclass__(cls, **kwargs):
-        cls.str_alias_cls[cls.__name__] = cls
-
-        super().__init_subclass__(**kwargs)
-
-    def __init__(self, k: int = None):
-        self.k = k
-
-    @property
-    def operator_comparison(self):
-
-        # What is the operator to use if we want to obtain the best result the metric?
-        # e.g. loss is "<", hit is ">", mse is "<", etc.
-        # By default is ">"
-        return operator.gt
-
-    @staticmethod
-    def rel_binary_matrix(predictions: np.ndarray[np.ndarray[str]], truths: np.ndarray[np.ndarray[str]], k: int = None):
-
-        # If K is none a new dimension is added! Important to be sure is != None
-        if k is not None:
-            predictions = predictions[:, k]
-
-        result = (predictions[:, np.newaxis, :] == truths[:, :, np.newaxis]) & \
-                 (predictions[:, np.newaxis, :] != "<PAD>")
-
-        rel_matrix = result.any(axis=1)
-
-        return rel_matrix
-
-    @staticmethod
-    def safe_div(num: np.ndarray, den: np.ndarray):
-
-        # divide only if denominator is different from 0, otherwise 0
-        return np.divide(num, den, out=np.zeros_like(num, dtype=float), where=den != 0)
-
-    @classmethod
-    def from_string(cls, *metric_str: str) -> List[Metric]:
-
-        instantiated_metrics = []
-        for metric in metric_str:
-
-            try:
-                metric_info = metric.split("@")
-
-                if len(metric_info) == 1:
-
-                    [metric_name] = metric_info
-
-                    instantiated_metrics.append(cls.str_alias_cls[metric_name]())
-                elif len(metric_info) == 2:
-                    [metric_name, k] = metric_info
-
-                    if not k.isdigit():
-                        raise KeyError
-
-                    instantiated_metrics.append(cls.str_alias_cls[metric_name](k=int(k)))
-
-            except KeyError:
-                raise KeyError(f"{metric} metric does not exist!") from None
-
-        return instantiated_metrics
-
-    @abstractmethod
-    def __call__(self, rel_binary_matrix: np.ndarray[np.ndarray[bool]]) -> float:
-        raise NotImplementedError
-
-    def __str__(self):
-        string = self.__class__.__name__
-        if self.k is not None:
-            string += f"@{self.k}"
-
-        return string
+from src.evaluate.abstract_metric import Metric
 
 
 class Loss(Metric):
