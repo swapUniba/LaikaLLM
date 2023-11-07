926a516590e7988a77a8d7310673d49556b97f90
Silleellie <antonio.silletti@gmail.com>
2023-11-07 12:22:05 +0100

    added rating prediction task

--- src/data/templates/templates.py
+++ src/data/templates/templates.py
@@ -4,6 +4,91 @@ import random
 import numpy as np
 
 from src.data.abstract_templates import Task, PromptTarget
+from src.evaluate.metrics.error_metrics import ErrorMetric
+from src.evaluate.metrics.ranking_metrics import RankingMetric
+
+
+class RatingPredictionTask(Task):
+    templates_dict = {
+        0: PromptTarget(
+            input_prompt="rating prediction - {user_id}: \n\n"
+                         "Can you predict the rating that the user would give to {target_item} knowing that "
+                         "it gave these ratings to these items? (Predict a number between 1 and 5 included) \n"
+                         "{order_history_w_ratings}",
+            target_text="{target_rating}"
+        ),
+        1: PromptTarget(
+            input_prompt="rating prediction - {user_id}: \n\n"
+                         "Predict a value between 1 and 5 which should be the rating that the user "
+                         "would give to {target_item}. As context, this is how the user rated previously bought "
+                         "item: \n"
+                         "{order_history_w_ratings}",
+            target_text="{target_rating}"
+        ),
+        2: PromptTarget(
+            input_prompt="rating prediction - {user_id}: \n\n"
+                         "How would the user rate in a 1-5 scale {target_item}? The user has bought these items "
+                         "and rated them in this way: \n"
+                         "{order_history_w_ratings}",
+            target_text="{target_rating}"
+        ),
+        3: PromptTarget(
+            input_prompt="rating prediction - {user_id}: \n\n"
+                         "Predict the score the user would give to {target_item} (in a 1-5 scale). This is its order "
+                         "history with associated rating that the user previously gave: \n"
+                         "{order_history_w_ratings}",
+            target_text="{target_rating}"
+        ),
+        4: PromptTarget(
+            input_prompt="rating prediction - {user_id}: \n\n"
+                         "This is the order history of the user with the associated rating -> \n"
+                         "{order_history_w_ratings} \n"
+                         "Based on that, predict the score (in a 1-5 scale) the user would give to {target_item}",
+            target_text="{target_rating}"
+        ),
+        5: PromptTarget(
+            input_prompt="rating prediction - {user_id}: \n\n"
+                         "Please predict the user would give to {target_item} based on its order history -> \n"
+                         "{order_history_w_ratings} \n"
+                         "The score should be in a 1-5 scale",
+            target_text="{target_rating}"
+        )
+    }
+
+    compatible_metrics = [ErrorMetric]
+
+    @property
+    def is_ranking_task(self) -> bool:
+        return False
+
+    def inference_templates(self, return_id: bool = False):
+        return self.all_templates(return_id)
+
+    @Task.validate_args("user_id", "input_item_seq", "input_rating_seq", "gt_item", "gt_rating")
+    def __call__(self, **kwargs):
+        assert len(kwargs["gt_item"]) == 1, "This task was designed for Leave One Out strategy!"
+
+        user_id = kwargs["user_id"]
+        order_history = kwargs["input_item_seq"]
+        rating_history = kwargs["input_rating_seq"]
+        [target_item] = kwargs["gt_item"]
+        [target_rating] = kwargs["gt_rating"]
+
+        # random.choice applied to dict with int key returns a value
+        input_prompt, target, _ = random.choice(self.all_templates())
+
+        # random select of string separator for item ids
+        separator = " , " if random.getrandbits(1) else " ; "
+
+        order_history_w_ratings = separator.join([f"{item_id} - {rating}"
+                                                  for item_id, rating in zip(order_history, rating_history)])
+
+        input_text = input_prompt.format(user_id=user_id,
+                                         target_item=target_item,
+                                         order_history_w_ratings=order_history_w_ratings)
+        target_text = target.format(target_rating=target_rating)
+
+        return [PromptTarget(input_text, target_text, gt=[target_rating])]
 
 
 class SequentialTask(Task):
